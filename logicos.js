97983bf6bcd6d8704c1ed722ff07ba8f09fa0d7f
João Paulo Vasconcelos <joaopaulov81@gmail.com>
2023-03-06 15:48:30 -0300

    mais itens

---
+++ logicos.js
@@ -0,0 +1,14 @@
+function compras (trabalho1, trabalho2){
+    const compraSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2;
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2)
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !compraSorvete;
+
+    return{compraSorvete, comprarTv50, comprarTv32, manterSaudavel}
+
+}
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
